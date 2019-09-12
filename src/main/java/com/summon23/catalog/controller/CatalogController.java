39bb37a82561960d0fd48f11dc86ddbda9ce2124
Andry Mahdison <andry.mahdison@bizzy.co.id>
2019-09-13 01:39:52 +0700

    add rest spring function

--- src/main/java/com/summon23/catalog/controller/CatalogController.java
+++ src/main/java/com/summon23/catalog/controller/CatalogController.java
@@ -1,15 +1,34 @@
 package com.summon23.catalog.controller;
 
+import com.summon23.catalog.entity.Catalog;
+import com.summon23.catalog.service.CatalogService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("catalog")
 public class CatalogController {
 
+    @Autowired
+    private final CatalogService catalogService;
+
+    public CatalogController(CatalogService catalogService) {
+        this.catalogService = catalogService;
+    }
+
     @GetMapping
-    public int getCatalogNum() {
-        return 1;
+    public List<Catalog> getAll() {
+        return catalogService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional getCatalogById(@PathVariable(value = "id") Long catalogId) {
+        return catalogService.findById(catalogId);
     }
 }
